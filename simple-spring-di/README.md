9e9c58a679c591fc699032c0c1940512bc161b44
yufarui <2412442175@qq.com>
2019-11-11 11:16:54 +0800

    spring-di update

--- simple-spring-di/README.md
+++ simple-spring-di/README.md
@@ -1,6 +1,13 @@
 # simple-spring-di
 > 研究spring依赖注入
 ## yu.problem
+```
+运行 yu.problem.Application 会抛出循环引用的依赖
+```
 > 产生了循环依赖,让我们debug代码并分析下原因
+
+```$xslt
+简单的写一个 产生此原因的demo
+```
 
 > 动手写一个类似框架的循环依赖解决方案
--- simple-spring-di/src/main/java/yu/problem/Application.java
+++ simple-spring-di/src/main/java/yu/problem/Application.java
@@ -1,11 +1,8 @@
-package yu;
+package yu.problem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
-import yu.problem.BeanB;
-import yu.problem.Config;
-import yu.problem.Service;
 
 @SpringBootApplication
 @Import({Service.class, Config.class, BeanB.class})
