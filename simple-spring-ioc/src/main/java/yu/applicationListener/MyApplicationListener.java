37d1bbc7e00c4356ca578812e3c1254e27dc39c5
yufarui <2412442175@qq.com>
2020-03-05 23:10:27 +0800

    spring ioc base

--- simple-spring-ioc/src/main/java/yu/applicationListener/MyApplicationListener.java
+++ simple-spring-ioc/src/main/java/yu/applicationListener/MyApplicationListener.java
@@ -2,12 +2,18 @@ package yu.applicationListener;
 
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyApplicationListener implements ApplicationListener {
 	@Override
 	public void onApplicationEvent(ApplicationEvent event) {
-		System.out.println("监听到事件" + event);
+		System.out.println("onApplicationEvent 监听到事件 " + event);
+	}
+
+	@EventListener
+	public void handle(ApplicationEvent event) {
+		System.out.println("@EventListener 监听到事件 " + event);
 	}
 }
--- simple-spring-ioc/src/main/java/yu/compentScanTest/CompentScanApp.java
+++ simple-spring-ioc/src/main/java/yu/compentScanTest/CompentScanApp.java
@@ -4,11 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
-import yu.compentScanTest.config.MainConfig;
 
 @SpringBootApplication
-@Import({MainConfig.class})
 @ComponentScan(useDefaultFilters = false)
 public class CompentScanApp {
     public static void main(String[] args) {
